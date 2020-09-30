8482e49fe1f1fd463c7b9c8a4215b2d2baeebaa4
attrishreyansh <67724480+attrishreyansh@users.noreply.github.com>
2020-09-30 19:04:43 +0530

    Update Tic-tac-toe.py

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from array import *
 window = Tk()
-window.title("Game")
+window.title("Welcome to a Game of TIC-TOC-TOE")
 window.wm_iconbitmap("tic-tac-toe_39453.ico")
 window.geometry("500x500")
 window.maxsize(500,500)
